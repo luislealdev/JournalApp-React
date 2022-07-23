04fe06afe97f52a65fa3b3963862c9c766837a55
luislealdev <luisrrleal@gmail.com>
2022-07-23 13:27:00 -0500

    Add loading jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,8 +1,13 @@
+import { useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
+import {Loading} from '../ui/components'
 
 export const AppRoutes = () => {
+
+  const {status} = useSelector(state=>state.auth)
+  if(status=='checking') return <Loading/>
   return (
     <Routes>
         <Route path="/auth/*" element={<AuthRoutes/>}/>
