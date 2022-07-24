916906c0369bbc7d4d0d1cac3101c77fdb2bda90
luislealdev <luisrrleal@gmail.com>
2022-07-24 00:19:39 -0500

    Add animations

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -7,7 +7,7 @@ export const JournalLayout = ({children}) => {
     const drawerWidth = 240;
 
   return (
-    <Box sx={{display: 'flex'}}>
+    <Box sx={{display: 'flex'}} className="animate__animated animate__fadeIn animate__faster">
         <NavBar drawerWidth={drawerWidth}/>
         <SideBar drawerWidth={drawerWidth}/>
 
