5b57904d2f5bee29d1d8ae94c1c37c2184cf7a9c
Luis Leal <luisrrleal@gmail.com>
2022-07-24 23:43:31 -0500

    Add a new note

--- src/journal/pages/HomePage.jsx
+++ src/journal/pages/HomePage.jsx
@@ -1,15 +1,25 @@
 import { AddOutlined } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { startNewNote } from "../../store/journal";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NothingSelectedView, NoteView } from "../views";
 
 export const HomePage = () => {
+
+  const dispatch = useDispatch();
+
+  const onNewNote = () => {
+    dispatch(startNewNote());
+  }
+
   return (
     <JournalLayout>
       {/*When Nothing */}
       {/* <NothingSelectedView/> */}
       <NoteView />
       <IconButton
+        onClick={onNewNote}
         size="large"
         sx={{
           color: "white",
--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,15 +1,21 @@
-export const startNewNote = ()=>{
-    return async (dispatch) => {
-        //uid
+import { collection, doc, setDoc } from "firebase/firestore/lite";
+import { firebaseDB } from "../../firebase/config";
 
-        const newNote = {
-            title:'',
-            body:'',
-            date: new Date().getTime(),
-        }
+export const startNewNote = () => {
+  return async (dispatch, getState) => {
+    const { uid } = getState().auth;
 
-        //dispatch
-        //dispatch(newNote)
-        //dispatch(activarNota)
-    }
-}
+    const newNote = {
+      title: "",
+      body: "",
+      date: new Date().getTime(),
+    };
+
+    const newDoc = doc(collection(firebaseDB, `${uid}/journal/notes`));
+    await setDoc(newDoc, newNote);
+
+    //dispatch
+    //dispatch(newNote)
+    //dispatch(activarNota)
+  };
+};
